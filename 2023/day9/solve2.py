009982c1f6d43bd4cdc1e1f71fbbb73684b54ca0
Matteo Bernabito <matteo@bernabito.it>
2023-12-09 17:16:00 +0100

    Add day 9 solutions

---
+++ 2023/day9/solve2.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def get_extrapolated_value(input: str) -> int:
+    nums = list(reversed(list(map(int, input.strip().split(' ')))))
+    print(nums)
+    diffs = [nums[i] - nums[i-1] for i in range(1, len(nums))]
+    diff_sum = diffs[-1]
+
+    while any(diffs[i] != diffs[0] for i in range(1, len(diffs))):
+        diffs = [diffs[i] - diffs[i-1] for i in range(1, len(diffs))]
+        diff_sum += diffs[-1]
+
+    return nums[-1] + diff_sum
+
+
+with open(sys.argv[1], 'r') as file:
+    print(sum(get_extrapolated_value(line) for line in file))
